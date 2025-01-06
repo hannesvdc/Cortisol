418010da595a977b1422eee2978578baf9373f0a
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-01-06 20:24:24 +0100

    Adding variables to MainActivity.kt

--- app/src/main/java/com/hannesvdc/cortisol/MainActivity.kt
+++ app/src/main/java/com/hannesvdc/cortisol/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.hannesvdc.cortisol
 
 import android.os.Bundle
+import android.os.CountDownTimer
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var textView4Hour: TextView
+    private lateinit var wakeButton: Button
+    private var timer: CountDownTimer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        // Initialize UI components
         setContentView(R.layout.activity_main)
-        findViewById<Button>(R.id.wake_button).setOnClickListener {
+        textView4Hour = findViewById(R.id.text_view_4_hour)
+        wakeButton = findViewById(R.id.wake_button)
+
+        wakeButton.setOnClickListener {
             Log.println(Log.INFO, "Button", "The user pressed the button")
         }
     }
