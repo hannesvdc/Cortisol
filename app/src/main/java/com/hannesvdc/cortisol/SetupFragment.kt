e7961d437e21990b4467189f1d41a99be44652d2
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-01-26 11:21:19 -0500

    Added chipgropu and continue button logic. This Fragment is now complete

--- app/src/main/java/com/hannesvdc/cortisol/SetupFragment.kt
+++ app/src/main/java/com/hannesvdc/cortisol/SetupFragment.kt
@@ -1,17 +1,19 @@
 package com.hannesvdc.cortisol
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.google.android.material.chip.Chip
+import com.google.android.material.chip.ChipGroup
 
 class SetupFragment : Fragment() {
 
     private lateinit var button : Button
-    private lateinit var addisonCheck : Chip
+    private lateinit var diseaseChipGroup : ChipGroup
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?) : View? {
         return inflater.inflate(R.layout.intro, container, false)
@@ -20,16 +22,20 @@ class SetupFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        addisonCheck = view.findViewById(R.id.addison_chip)
-        addisonCheck.setOnClickListener {
-            button.isEnabled = addisonCheck.isChecked
+        diseaseChipGroup = view.findViewById(R.id.disease_chipgroup)
+        diseaseChipGroup.setOnCheckedStateChangeListener { _, checkedIds ->
+            Log.i("ChipGroup", "A checkbox has changed")
+            val anyChecked = checkedIds.isNotEmpty()
+
+            button.isEnabled = anyChecked
         }
 
         button = view.findViewById(R.id.continue_button)
         button.setOnClickListener {
             // Gather all Preferences
             val preferences = Bundle()
-            preferences.putBoolean("Addison", addisonCheck.isChecked)
+            preferences.putBoolean("Addison", (diseaseChipGroup.getChildAt(0) as Chip).isChecked)
+            preferences.putBoolean("Other", (diseaseChipGroup.getChildAt(1) as Chip).isChecked)
 
             // Get a reference to the MainActivity and trigger navigation
             (activity as? MainActivity)?.navigateToMainFragment(preferences)
